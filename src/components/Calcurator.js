02e1d4e8c386ddbc80465b6909a259edff156c57
Fatima Ezzahra <felemenoun@gmail.com>
2022-02-09 18:59:32 +0100

    Add functionality to the calculator

--- src/components/Calcurator.js
+++ src/components/Calcurator.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Calculator.css';
+import calculate from './logic/calculate';
 
 class Calculator extends React.Component {
   constructor(props) {
@@ -7,12 +8,19 @@ class Calculator extends React.Component {
     this.state = {
       output: 0,
     };
+    this.obj = {
+      total: null,
+      next: null,
+      operation: null,
+    };
   }
 
   clickHandler = (e) => {
     this.newOutput = e.target.textContent;
+    this.obj = calculate(this.obj, this.newOutput.toString());
+    console.log(this.obj);
     this.setState({
-      output: this.newOutput,
+      output: this.obj.total,
     });
   };
 
@@ -23,25 +31,25 @@ class Calculator extends React.Component {
         <div className="output">{output}</div>
         <div className="numbers">
           <button type="button" onClick={this.clickHandler}>7</button>
-          <button type="button">8</button>
-          <button type="button">9</button>
-          <button type="button">4</button>
-          <button type="button">5</button>
-          <button type="button">6</button>
-          <button type="button">1</button>
-          <button type="button">2</button>
-          <button type="button">3</button>
-          <button type="button" className="number0">0</button>
-          <button type="button">.</button>
+          <button type="button" onClick={this.clickHandler}>8</button>
+          <button type="button" onClick={this.clickHandler}>9</button>
+          <button type="button" onClick={this.clickHandler}>4</button>
+          <button type="button" onClick={this.clickHandler}>5</button>
+          <button type="button" onClick={this.clickHandler}>6</button>
+          <button type="button" onClick={this.clickHandler}>1</button>
+          <button type="button" onClick={this.clickHandler}>2</button>
+          <button type="button" onClick={this.clickHandler}>3</button>
+          <button type="button" className="number0" onClick={this.clickHandler}>0</button>
+          <button type="button" onClick={this.clickHandler}>.</button>
         </div>
-        <button type="button" className="operation">&#xf7;</button>
-        <button type="button" className="operation">x</button>
-        <button type="button" className="operation">-</button>
-        <button type="button" className="operation">+</button>
-        <button type="button" className="operation">=</button>
-        <button type="button" className="options">AC</button>
-        <button type="button" className="options">+/-</button>
-        <button type="button" className="options">%</button>
+        <button type="button" className="operation" onClick={this.clickHandler}>&#xf7;</button>
+        <button type="button" className="operation" onClick={this.clickHandler}>x</button>
+        <button type="button" className="operation" onClick={this.clickHandler}>-</button>
+        <button type="button" className="operation" onClick={this.clickHandler}>+</button>
+        <button type="button" className="operation" onClick={this.clickHandler}>=</button>
+        <button type="button" className="options" onClick={this.clickHandler}>AC</button>
+        <button type="button" className="options" onClick={this.clickHandler}>+/-</button>
+        <button type="button" className="options" onClick={this.clickHandler}>%</button>
       </div>
     );
   }
