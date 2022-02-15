7a9003a1b8da4e7b21838ae709073e8d3401cfe1
Fatima Ezzahra <felemenoun@gmail.com>
2022-02-15 21:11:39 +0100

    Add style to the app pages

--- src/pages/CalculatorPage.js
+++ src/pages/CalculatorPage.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Calculator from '../components/Calculator';
 
 const CalculatorPage = () => (
-  <div>
+  <div className="calculator_content d-flex-row">
     <h1>Let&apos;s do some math!</h1>
     <Calculator />
   </div>
